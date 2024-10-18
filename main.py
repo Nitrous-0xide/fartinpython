20418efaf4d2c98a735fc9bbafe1c45ceb3b1d19
Nitrous-Oxide <160649787+Nitrous-0xide@users.noreply.github.com>
2024-10-18 12:28:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("fart")
+tripname = ["Lucien","is","really"]
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+x = " ".join(tripname)
+pepperonicounter = [x]
+if pepperonicounter[0] == " ".join(tripname):
+    pepperoniboy = True
+    
+
+while pepperoniboy == True:
+        alphjoin = (x,alphabet[5]+alphabet[17]+alphabet[20]+alphabet[8]+alphabet[19]+alphabet[24])
+        thealphbetjoined = " ".join(alphjoin)
+        pepperonicounter.append(thealphbetjoined)
+        
+        print(pepperonicounter[1])
+
+        
