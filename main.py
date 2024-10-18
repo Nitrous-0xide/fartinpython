ab1aba09727fbe3abc8c29706e6364cd482613d6
Nitrous-Oxide <160649787+Nitrous-0xide@users.noreply.github.com>
2024-10-18 12:36:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-tripname = ["Lucien","is","really"]
+
+
+tripname = ["Louis's","group","is","really"]
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 x = " ".join(tripname)
 pepperonicounter = [x]
@@ -6,11 +8,11 @@ if pepperonicounter[0] == " ".join(tripname):
     pepperoniboy = True
     
 
-while pepperoniboy == True:
+for letters in (alphabet):
         alphjoin = (x,alphabet[5]+alphabet[17]+alphabet[20]+alphabet[8]+alphabet[19]+alphabet[24])
         thealphbetjoined = " ".join(alphjoin)
         pepperonicounter.append(thealphbetjoined)
         
-        print(pepperonicounter[1])
+        print(pepperonicounter[1],"(Message sent by: Annonymous. Code hidden by Matthew)")
 
         
