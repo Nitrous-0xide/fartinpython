6514cbdf3e4a0589b0ab1d957ba524e809632c4e
Nitrous-Oxide <160649787+Nitrous-0xide@users.noreply.github.com>
2024-10-21 12:57:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
+import hashlib
 
-
-tripname = ["Louis's","group","is","really"]
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ghg = "s"
+currenthash = "sha256"
+tripname = [""]
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 x = " ".join(tripname)
 pepperonicounter = [x]
+
 if pepperonicounter[0] == " ".join(tripname):
     pepperoniboy = True
-    
-
-for letters in (alphabet):
-        alphjoin = (x,alphabet[5]+alphabet[17]+alphabet[20]+alphabet[8]+alphabet[19]+alphabet[24])
+    for letter in ghg:
+        alphjoin = (x, alphabet[3] + alphabet[8] + alphabet[4])
         thealphbetjoined = " ".join(alphjoin)
+        sha256 = hashlib.sha256()
+        sha256.update(thealphbetjoined.encode('utf-8'))
+        string_hash = sha256.hexdigest()
         pepperonicounter.append(thealphbetjoined)
-        
-        print(pepperonicounter[1])
 
-        
+        print(f"Hashed message in {currenthash}")
+        print("Hashed: "+string_hash)
+        print("Dehashed:"+pepperonicounter[1])
